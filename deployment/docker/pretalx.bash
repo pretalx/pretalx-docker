bc79de19b6ad1afd8d7e245371937c657b37b895
Luca <Luca@hackerspace-bamberg.de>
2023-12-17 02:56:58 +0100

    Docker: add env var for Gunicorn forwarded-allow-ips

--- deployment/docker/pretalx.bash
+++ deployment/docker/pretalx.bash
@@ -8,6 +8,7 @@ export HOME=/pretalx
 export GUNICORN_WORKERS="${GUNICORN_WORKERS:-${WEB_CONCURRENCY:-$((2 * $(nproc)))}}"
 export GUNICORN_MAX_REQUESTS="${GUNICORN_MAX_REQUESTS:-1200}"
 export GUNICORN_MAX_REQUESTS_JITTER="${GUNICORN_MAX_REQUESTS_JITTER:-50}"
+export GUNICORN_FORWARDED_ALLOW_IPS="${FORWARDED_ALLOW_IPS:-127.0.0.1}"
 
 AUTOMIGRATE="${AUTOMIGRATE:-yes}"
 
@@ -36,6 +37,7 @@ if [ "$1" == "webworker" ]; then
         --workers "${GUNICORN_WORKERS}" \
         --max-requests "${GUNICORN_MAX_REQUESTS}" \
         --max-requests-jitter "${GUNICORN_MAX_REQUESTS_JITTER}" \
+        --forwarded-allow-ips "${GUNICORN_FORWARDED_ALLOW_IPS}" \
         --log-level=info \
         --bind=0.0.0.0:80
 fi
