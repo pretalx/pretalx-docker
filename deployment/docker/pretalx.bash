a4081b8f57a7516934ce1a074480ba17464c85e5
Matt Yaraskavitch <62650344+yaraskm@users.noreply.github.com>
2024-04-03 18:09:46 -0400

    55: Allow changing the listen port for gunicorn
    - This PR will resolve issue #55 by allowing admins to configure
    gunicorn to listen on any non-privileged port (>1024). Listening on
    privileged ports can cause issues on hardended Kubernetes clusters and
    runtimes such as Podman, as discussed in a [related Kubernetes issue](https://github.com/kubernetes/kubernetes/issues/56374)

--- deployment/docker/pretalx.bash
+++ deployment/docker/pretalx.bash
@@ -11,6 +11,7 @@ GUNICORN_WORKERS="${GUNICORN_WORKERS:-${WEB_CONCURRENCY:-$((2 * $(nproc)))}}"
 GUNICORN_MAX_REQUESTS="${GUNICORN_MAX_REQUESTS:-1200}"
 GUNICORN_MAX_REQUESTS_JITTER="${GUNICORN_MAX_REQUESTS_JITTER:-50}"
 GUNICORN_FORWARDED_ALLOW_IPS="${GUNICORN_FORWARDED_ALLOW_IPS:-127.0.0.1}"
+GUNICORN_BIND_ADDR="${GUNICORN_BIND_ADDR:-0.0.0.0:80}"
 
 AUTOMIGRATE="${AUTOMIGRATE:-yes}"
 AUTOREBUILD="${AUTOREBUILD:-yes}"
@@ -58,7 +59,7 @@ if [ "$1" == "webworker" ]; then
         --max-requests-jitter "${GUNICORN_MAX_REQUESTS_JITTER}" \
         --forwarded-allow-ips "${GUNICORN_FORWARDED_ALLOW_IPS}" \
         --log-level=info \
-        --bind=0.0.0.0:80
+        --bind="${GUNICORN_BIND_ADDR}"
 fi
 
 if [ "$1" == "taskworker" ]; then
